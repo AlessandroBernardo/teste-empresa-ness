76e3e20de6588b3828372211b1d6a2875f724134
Alessandro Bernardo <alessandro_bernardo@outlook.com.br>
2019-02-14 01:33:46 -0200

    fix

--- TesteEmpresaNess/Startup.cs
+++ TesteEmpresaNess/Startup.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TesteNess.Models;
 
-namespace TesteEmpresaNess
+namespace TesteNess
 {
     public class Startup
     {
@@ -33,6 +34,9 @@ namespace TesteEmpresaNess
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ClasseContexto>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,10 +49,8 @@ namespace TesteEmpresaNess
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
